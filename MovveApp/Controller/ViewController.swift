e64b50e0754855bcf1c2ffc6e91815e372c573f6
spviz <spviz.work@gmail.com>
2022-08-31 23:21:38 +0300

    Add cells

--- MovveApp/Controller/ViewController.swift
+++ MovveApp/Controller/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  MovveApp
-//
-//  Created by Артем Орлов on 28.08.2022.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table : UITableView!
+    
+    var models = [Model]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.register(CollectionTableViewCell.nib(), forCellReuseIdentifier: CollectionTableViewCell.identifier)
+        table.delegate = self
+        table.dataSource = self
+        
+        models.append(Model(text: "First", imageName: "Image_1"))
+        models.append(Model(text: "Second", imageName: "Image_2"))
+        models.append(Model(text: "Third", imageName: "Image_3"))
+        models.append(Model(text: "Forth", imageName: "Image_4"))
+
     }
+    
+    //Tableview
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: CollectionTableViewCell.identifier, for: indexPath) as! CollectionTableViewCell
+        cell.configure(with: models)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350
+    }
+    
 
+}
 
+struct Model {
+    let text: String
+    let imageName: String
 }
 
